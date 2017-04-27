07d4ae4d9a79376f21de14cda0027b202e0f5141
BraedenRichards <BraedenRichards@gmail.com>
2017-04-27 10:40:02 -0700

    Created the readme and created the main.cpp file which currently is empty other than the main() function that has nothing in it.

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/**
+	JSON Communicator Main File
+	Purpose: To be able to send and receive JSON information on a player class
+	
+	Written  by Braeden Richards
+	Created: April 27th, 2017
+*/
+
+/**
+	Main file is Dependent on:
+*/
+
+#include <iostream>
+
+
+
+
+/**
+	Main file, runs the program.
+
+	@param void
+	@return 0
+*/
+int main(void)
+{
+
+
+	return 0;
+}
