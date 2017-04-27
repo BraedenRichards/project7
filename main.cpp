99c313976d3358e5eb12dabe0ad80c85b15c2f42
BraedenRichards <BraedenRichards@gmail.com>
2017-04-27 16:08:32 -0700

    Got main function to read in all data from PlayerStatistics.json

--- main.cpp
+++ main.cpp
@@ -10,10 +10,15 @@
 	Main file is Dependent on:
 */
 
+
+//Libraries
 #include <iostream>
+#include <fstream>
+#include <string>
 
 
-
+//Prototypes
+void readInData(char fileName[], std::string* data);
 
 /**
 	Main file, runs the program.
@@ -23,7 +28,35 @@
 */
 int main(void)
 {
+	std::string test;
+	readInData("PlayerStatistics.json", &test);
 
+	std::cout << test << "\n";
 
 	return 0;
+}
+
+
+
+/**
+	Temprorary function that reads in the JSON data
+
+	@param char array of the file name to open, string to read the data to
+	@return void
+*/
+void readInData(char fileName[], std::string* data)
+{
+	std::ifstream inFile(fileName);
+
+	if(inFile.is_open())
+	{
+		while(inFile.good())
+		{
+			std::string temp;
+			inFile >> temp;
+			*data += " " + temp;
+		}
+	}
+
+	return;
 }
